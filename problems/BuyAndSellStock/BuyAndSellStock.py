39dd72d77551d90b3d638a773da66421a92e7ec9
nikoa-a <niko.alaaho@gmail.com>
2024-03-12 13:27:05 +0200

    Best Time to Buy and Sell Stock solved

--- problems/BuyAndSellStock/BuyAndSellStock.py
+++ problems/BuyAndSellStock/BuyAndSellStock.py
@@ -2,4 +2,18 @@ class BuyAndSellStock (object):
 
     def buyAndSellStock(self, prices):
 
-        return
+        profit = 0 # Variable to store the maximum profit
+        min = prices[0] # Variable to store the minimum price, initialized with the first price
+
+        # Loop through the prices starting from the second price, saving the current price in the variable 'price'
+        for price in prices[1:]:
+            # Check if the current price is greater than the minimum price
+            if price > min:
+                # Calculate the profit and update the maximum profit, if necessary
+                profit = max(profit, price - min)
+            # If the current price is less than the minimum price, update the minimum price
+            else:
+                min = price
+
+        # Return the maximum profit
+        return profit
