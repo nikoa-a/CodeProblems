94cb350b402a83366ecc3f91f2c5e85b3a8f57c2
nikoa-a <niko.alaaho@gmail.com>
2024-03-21 12:51:11 +0200

    Solve Linked List Cycle problem

--- problems/LinkedListCycle/LinkedListCycle.py
+++ problems/LinkedListCycle/LinkedListCycle.py
@@ -8,4 +8,18 @@ class LinkedListCycle:
 
     def hasCycle(self, head):
 
-        return True
+        # Initialize slow and fast pointers. Slow pointer moves one step at a time while fast pointer moves two steps at a time.
+        # 'fast' is fast pointer and 'head' is slow pointer.
+        fast = head
+        # Loop through the linked list until fast pointer reaches the end of the list.
+        while fast and fast.next:
+            # Move slow pointer one step at a time.
+            head = head.next
+            # Move fast pointer two steps at a time.
+            fast = fast.next.next
+            # If fast pointer meets slow pointer, a cycle is detected.
+            if head is fast:
+                return True
+
+        # If fast pointer reaches the end of the list, then there is no cycle in the linked list.
+        return False
--- tests/test_LinkedListCycle.py
+++ tests/test_LinkedListCycle.py
@@ -8,4 +8,30 @@ def test_linked_list_cycle():
     llc_instance = LinkedListCycle()
 
     # Test case 1
-    # Tests here
+    head = ListNode(3)
+    node2 = ListNode(2)
+    node3 = ListNode(0)
+    node4 = ListNode(-4)
+
+    head.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node2
+
+    expected = True
+
+    assert llc_instance.hasCycle(head) == expected
+
+    # Test case 2
+    head = ListNode(3)
+    node2 = ListNode(2)
+    node3 = ListNode(0)
+    node4 = ListNode(-4)
+
+    head.next = node2
+    node2.next = node3
+    node3.next = node4
+
+    expected = False
+
+    assert llc_instance.hasCycle(head) == expected
