12d5cb5859fbf0e95dc25b83ddd66d4d3b1d78b7
nikoa-a <niko.alaaho@gmail.com>
2024-03-21 14:04:04 +0200

    Solve Counting Bits problem

--- problems/CountingBits/CountingBits.py
+++ problems/CountingBits/CountingBits.py
@@ -1,5 +1,15 @@
 class CountingBits (object):
 
-    def countBits(self, int):
+    def countBits(self, n):
 
-        return 0
+        # Initialize an array to store the number of set (1) bits for each number from 0 to n
+        ans = [0] * (n +1)
+
+        # Iterate through the numbers from 1 to n 
+        for i in range(1, n + 1):
+            # Count the set bits for the right-shifted counterpart of i
+            # and add the count of the least significant bit of i
+            ans[i] = ans[i >> 1] + (i & 1)
+
+        # Return the array of set bits
+        return ans
