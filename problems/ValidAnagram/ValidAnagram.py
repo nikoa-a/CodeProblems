dea4ec8e545b9534d15456f1e059339ddb9435ed
nikoa-a <niko.alaaho@gmail.com>
2024-03-22 11:45:35 +0200

    Solve Valid Anagram problem

--- problems/ValidAnagram/ValidAnagram.py
+++ problems/ValidAnagram/ValidAnagram.py
@@ -2,4 +2,23 @@ class ValidAnagram (object):
 
     def isAnagram(self, s, t):
 
-        return True
+        # If the length of the strings are different, they can't be anagrams
+        if len(s) != len(t):
+            return False
+        
+        # Create two dictionaries to store the frequency of each character in the strings
+        s_freq = {}
+        t_freq = {}
+
+        # Iterate trough the characters of the first string
+        for char in s:
+            # If the character is not in the dictionary, add it with a frequency of 1, otherwise increment the frequency by 1
+            s_freq[char] = s_freq.get(char, 0) + 1
+
+        # Iterate trough the characters of the second string
+        for char in t:
+            # If the character is not in the dictionary, add it with a frequency of 1, otherwise increment the frequency by 1
+            t_freq[char] = t_freq.get(char, 0) + 1
+
+        # If the dictionaries are equal, the strings are anagrams
+        return s_freq == t_freq
