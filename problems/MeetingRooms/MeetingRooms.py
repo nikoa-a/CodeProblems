46de8a3a679a3f106f32266e1bcc02df8cfdd390
nikoa-a <niko.alaaho@gmail.com>
2024-03-25 14:17:57 +0200

    Add Interval class to Meeting Rooms problem

--- problems/MeetingRooms/MeetingRooms.py
+++ problems/MeetingRooms/MeetingRooms.py
@@ -1,3 +1,11 @@
+class Interval(object):
+
+    def __init__(self, start, end):
+
+        self.start = start
+        self.end = end
+
+
 class MeetingRooms (object):
 
     def canAttendMeetings(self, intervals):
