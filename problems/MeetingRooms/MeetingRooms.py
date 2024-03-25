71aeccb7e09ca0ae54fcbd8c60b383403c90e6d2
nikoa-a <niko.alaaho@gmail.com>
2024-03-25 14:47:36 +0200

    Solve Meeting Rooms problem

--- problems/MeetingRooms/MeetingRooms.py
+++ problems/MeetingRooms/MeetingRooms.py
@@ -10,4 +10,14 @@ class MeetingRooms (object):
 
     def canAttendMeetings(self, intervals):
 
+        # Sort the intervals by start time
+        intervals.sort(key = lambda i : i.start)
+
+        # Iterate through the intervals starting from the second one
+        for i in range(1, len(intervals)):
+            # If the start time of the current interval is less than the end time of the previous interval, return False
+            if intervals[i].start < intervals[i-1].end:
+                return False
+
+        # If no overlapping intervals are found, return True
         return True
--- tests/test_MeetingRooms.py
+++ tests/test_MeetingRooms.py
@@ -1,17 +1,20 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from problems.MeetingRooms.MeetingRooms import MeetingRooms
+from problems.MeetingRooms.MeetingRooms import Interval, MeetingRooms
 
 def test_meeting_rooms():
     meeting_rooms_instance = MeetingRooms()
 
     # Test case 1
-    intervals = [(0,30),(5,10),(15,20)]
+    interval1 = Interval(0,30)
+    interval2 = Interval(5,10)
+    interval3 = Interval(15,20)
     expected = False
-    assert meeting_rooms_instance.canAttendMeetings(intervals) == expected
+    assert meeting_rooms_instance.canAttendMeetings([interval1, interval2, interval3]) == expected
 
     # Test case 2
-    intervals = [(5,8),(9,15)]
+    interval1 = Interval(5,8)
+    interval2 = Interval(9,15)
     expected = True
-    assert meeting_rooms_instance.canAttendMeetings(intervals) == expected
+    assert meeting_rooms_instance.canAttendMeetings([interval1, interval2]) == expected
