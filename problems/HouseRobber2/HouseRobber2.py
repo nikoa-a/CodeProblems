07059183cdf5cb238026c362e2f841cd7d73d234
nikoa-a <niko.alaaho@gmail.com>
2024-04-23 14:43:20 +0300

    Solve House Robber 2 problem

--- problems/HouseRobber2/HouseRobber2.py
+++ problems/HouseRobber2/HouseRobber2.py
@@ -2,4 +2,23 @@ class HouseRobber2 (object):
 
     def rob(self, nums):
 
-        return 0
+        # nums[0] is needed to handle the edge case where there is only one house
+        # robHelper(nums[1:]) robs all houses except the first house
+        # robHelper(nums[:-1]) robs all houses except the last house
+        # Return the max amount of money that can be robbed from the three cases
+        return max(nums[0], self.robHelper(nums[1:]), self.robHelper(nums[:-1]))
+    
+
+
+    # Helper function to calculate the max amount of money that can be robbed
+    # Code from first HouseRobber problem
+    def robHelper(self, nums):
+        rob1 = 0
+        rob2 = 0
+
+        for num in nums:
+            temp = max(num + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+
+        return rob2
