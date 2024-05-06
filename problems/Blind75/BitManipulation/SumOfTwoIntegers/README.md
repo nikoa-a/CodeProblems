6cc343ec4acb69ee1e6ef4e1853d51d47d59ddec
nikoa-a <niko.alaaho@gmail.com>
2024-05-06 12:44:10 +0300

    Solve Sum of Two Integers problem

--- problems/Blind75/BitManipulation/SumOfTwoIntegers/README.md
+++ problems/Blind75/BitManipulation/SumOfTwoIntegers/README.md
@@ -11,3 +11,23 @@ Given two integers a and b, return the sum of the two integers without using the
 #### Example 2:
 > **Input:** a = 2, b = 3<br>
 > **Output:** 5
+
+---
+
+This has more simple solution with Java that can not be achieved in Python:
+```java
+public int getSum(int a, int b) {
+
+    // Loop until the carry value ends up being 0
+    while (b != 0) {
+        // Calculate the carry bit and shift it to the left
+        int carry = (a & b) << 1;
+        // Sum without considering the carry
+        a = a ^ b;
+        // Update 'b' to hold the carry for the next iteration
+        b = carry;
+    }
+    // Return the final sum
+    return a;
+}
+```
--- problems/Blind75/BitManipulation/SumOfTwoIntegers/SumOfTwoIntegers.py
+++ problems/Blind75/BitManipulation/SumOfTwoIntegers/SumOfTwoIntegers.py
@@ -2,4 +2,24 @@ class SumOfTwoIntegers (object):
 
     def getSum(self, a, b):
 
-        return 0
+        # Define a recursive function to perform addition without using the '+' operator
+        def add(a, b):
+            # If either 'a' or 'b' is 0, return the non-zero value
+            if not a or not b:
+                return a or b
+            # Recursively add the sum of the bitwise XOR and the carry shifted to the left by 1
+            return add(a ^ b, (a & b) << 1)
+
+        # If both 'a' and 'b' have different signs
+        if a * b < 0: 
+            # Swap 'a' and 'b' if 'a' is positive
+            if a > 0:
+                return self.getSum(b, a)
+            # Check for overflow or underflow conditions
+            if add(~a, 1) == b:
+                return 0
+            if add(~a, 1) < b:  
+                return add(~add(add(~a, 1), add(~b, 1)), 1) 
+            
+        # Return the sum using the defined add function
+        return add(a, b)
