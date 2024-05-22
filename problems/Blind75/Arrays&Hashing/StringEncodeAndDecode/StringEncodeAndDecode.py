ca2d68499140ba19d5f401b196e9a7a037d9a7bb
nikoa-a <niko.alaaho@gmail.com>
2024-05-22 14:38:37 +0300

    Solve Encode and Decode Strings problem

--- problems/Blind75/Arrays&Hashing/StringEncodeAndDecode/StringEncodeAndDecode.py
+++ problems/Blind75/Arrays&Hashing/StringEncodeAndDecode/StringEncodeAndDecode.py
@@ -1,12 +1,44 @@
 class StringEncodeAndDecode (object):
 
+    # The shift value is used to encode and decode the strings
+    shift = 3
 
-    def encode(strs):
+    def encode(self, strs):
 
-        return ""
+        # Initialize the encoded string
+        encodedString = ""
+        # Iterate through the strings
+        for s in strs:
+            # Encode each character in the string
+            for char in s:
+                # Shift the character by the shift value
+                encodedChar = chr(ord(char) + self.shift)
+                # Add the encoded character to the encoded string
+                encodedString += encodedChar
+            # Add a null character to separate the strings
+            encodedString += '\0'
+
+        # Return the encoded string
+        return encodedString
     
     
-    def decode(s):
+    def decode(self, s):
 
-        return [""]
-    
+        # Initialize the list of decoded strings
+        decodedStringsList = []
+        # Initialize the decoded string
+        decodedString = ""
+        # Iterate through the encoded string
+        for char in s:
+            # Check if the character is a null character
+            if char == "\0":
+                # Add the decoded string to the list of decoded strings
+                decodedStringsList.append(decodedString)
+                decodedString = ""
+            # Decode the character
+            else:
+                decodedChar = chr(ord(char) - self.shift)
+                decodedString += decodedChar
+
+        # Return the list of decoded strings
+        return decodedStringsList
